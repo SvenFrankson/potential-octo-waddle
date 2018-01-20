8ca49786cb92c9e9cb4d337ca685116fdd25dfae
SvenFrankson <sven.frankson@gmail.com>
2018-01-20 19:50:22 +0100

    Generate Level icons with level index.

--- PotentialOctoWaddle/Assets/Scripts/LevelButton.cs
+++ PotentialOctoWaddle/Assets/Scripts/LevelButton.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelButton : Button3D {
-	public int index;
+
+	private int _index;
+	public int index {
+		get {
+			return this._index;
+		}
+		set {
+			this._index = value;
+			this.textMesh.text = "" + this._index;
+		}
+	}
+
+	public TextMeshPro textMesh;
+	public GameObject star1;
+	public GameObject star2;
+	public GameObject star3;
 
 	public void OnMouseDown() {
 		Debug.Log ("LevelButton Click");
