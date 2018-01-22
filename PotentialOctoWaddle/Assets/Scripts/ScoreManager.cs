30724fa143f44823aec7f745288d9520760c6806
SvenFrankson <sven.frankson@gmail.com>
2018-01-22 22:33:16 +0100

    Score Manager (no compile)

---
+++ PotentialOctoWaddle/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class ScoreData {
+	public int[] scores = new int[0];
+}
+
+public class ScoreManager {
+
+	public int GetScore(int index) {
+
+	}
+
+	public void SetScore(int index, int score) {
+
+	}
+}
